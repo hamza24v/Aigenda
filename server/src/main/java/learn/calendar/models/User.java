83ce934e57461223de4d1146a0b7a084a134e55a
Marcus Lim <mj_lim0802@yahoo.com>
2024-09-23 10:36:18 -0500

    updated pom file, made some models

---
+++ server/src/main/java/learn/calendar/models/User.java
@@ -0,0 +1,11 @@
+package learn.calendar.models;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
