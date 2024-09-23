0ef88bbd2bb796fe5f281bfd8b18de95e41d5171
Tyler Ovenden <tylerovenden@gmail.com>
2024-09-23 14:35:04 -0400

    fixed attendeemapper's way of set event attribute

--- server/src/main/java/learn/calendar/data/mappers/AttendeeMapper.java
+++ server/src/main/java/learn/calendar/data/mappers/AttendeeMapper.java
@@ -8,20 +8,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AttendeeMapper implements RowMapper<Attendee> {
-    private final EventMapper eventMapper;
-
-    public AttendeeMapper(EventMapper eventMapper) {
-        this.eventMapper = eventMapper;
-    }
-
+    
     @Override
     public Attendee mapRow(ResultSet resultSet, int i) throws SQLException {
         Attendee attendee = new Attendee();
         attendee.setId(resultSet.getInt("attendee_id"));
         attendee.setStatus(resultSet.getString("status"));
-        int eventId = resultSet.getInt("event_id");
-        Event event = eventMapper.mapRow(resultSet, i);
-        attendee.setEvent(event);
+        EventMapper eventMapper = new EventMapper();
+        attendee.setEvent(eventMapper.mapRow(resultSet,i));
         attendee.setUserId(resultSet.getInt("app_user_id"));
         return attendee;
     }
