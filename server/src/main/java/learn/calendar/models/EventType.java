03b01c382242414f3bbc4fe3cf16c67d315364fb
Tyler Ovenden <tylerovenden@gmail.com>
2024-09-23 11:50:24 -0400

    implemented enums

--- server/src/main/java/learn/calendar/models/EventType.java
+++ server/src/main/java/learn/calendar/models/EventType.java
@@ -1,4 +1,13 @@
 package learn.calendar.models;
 
 public enum EventType {
+
+    PERSONAL("Personal"),
+    ORGANIZATION("Organization");
+
+    private final String type;
+
+    EventType(String type) {
+        this.type = type;
+    }
 }
--- server/src/main/java/learn/calendar/models/RoleType.java
+++ server/src/main/java/learn/calendar/models/RoleType.java
@@ -1,4 +1,13 @@
 package learn.calendar.models;
 
 public enum RoleType {
+
+    ADMIN("Admin"),
+    USER("User");
+
+    private final String role;
+
+    RoleType(String role ) {
+        this.role = role;
+    }
 }
