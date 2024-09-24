701e67548de5980784bd2b74d3b4fdecf0444c2b
Tyler Ovenden <tylerovenden@gmail.com>
2024-09-24 17:47:35 -0400

    created controllers for invite & event

---
+++ server/src/main/java/learn/calendar/controller/EventController.java
@@ -0,0 +1,62 @@
+package learn.calendar.controller;
+
+import learn.calendar.domain.EventService;
+import learn.calendar.domain.Result;
+import learn.calendar.models.Event;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/event")
+public class EventController {
+
+    private final EventService service;
+
+    public EventController(EventService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Event> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<Event> findById(@PathVariable int eventId) {
+        Event event = service.findById(eventId);
+        if (event != null) {
+            return new ResponseEntity<>(event, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Event event) {
+        Result<Event> result = service.add(event);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> update(@RequestBody Event event) {
+        Result<Event> result = service.update(event);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.OK);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{eventId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int eventId) {
+        if (service.deleteById(eventId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
