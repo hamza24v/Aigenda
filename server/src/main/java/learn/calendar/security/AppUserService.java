1053688439c1239f5bc092c1fcc26e4c79ecff28
Marcus Lim <mj_lim0802@yahoo.com>
2024-09-24 11:52:55 -0500

    added usercalendarrole service and security stuff

---
+++ server/src/main/java/learn/calendar/security/AppUserService.java
@@ -0,0 +1,32 @@
+//package learn.calendar.security;
+//
+//
+//import learn.calendar.data.AppUserRepository;
+//import learn.calendar.models.AppUser;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AppUserService implements UserDetailsService {
+//    private final AppUserRepository repository;
+//    private final PasswordEncoder encoder;
+//
+//    public AppUserService(AppUserRepository repository, PasswordEncoder encoder) {
+//        this.repository = repository;
+//        this.encoder = encoder;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        AppUser appUser = repository.findByUsername(username);
+//
+//        if (appUser == null || !appUser.isEnabled()) {
+//            throw new UsernameNotFoundException(username + "not found");
+//        }
+//
+//        return appUser;
+//    }
+//}
