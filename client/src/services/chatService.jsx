fec3d5d3ccff7f2cb9cc88ccaf457f43d45f7274
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-09-25 20:42:04 -0400

    added chatbot

---
+++ client/src/services/chatService.jsx
@@ -0,0 +1,38 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+const systemPrompt = `You are a schedule manager assistant who's job is to schedule events and create calendar, and answer schedule related questions to user
+ When asked to create an event or calendar, you will respond by returning the following JSON object:
+ 
+ calendar: {
+  "title": str,
+  "type": str,
+ }
+ event: {
+  "title": str,
+  "start_date": datetime-local,
+  "end_date": datetime-local,
+  "description": str
+ }
+    datatime-local follows the js input tag format of "YYYY-MM-DDTHH:mm"
+  assume end date is one hour after start by default if end date is not given.
+
+`;
+export const sendMessage = async (message) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: systemPrompt },
+      { role: "user", content: message },
+    ],
+    model: "gpt-4o-mini",
+    response_format: { type: "json_object" },
+  });
+
+  const response = JSON.parse(completion.choices[0].message.content);
+
+  return response;
+};
